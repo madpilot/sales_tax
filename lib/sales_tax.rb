aae9dc599ec011cb2bef0d51bab6d42324e8b3f3
Myles Eftos <myles@madpilot.com.au>
2014-08-20 18:39:42 +1000

    Using a percent literal to load files

--- lib/sales_tax.rb
+++ lib/sales_tax.rb
@@ -4,7 +4,7 @@ Bundler.require
 # Eliminate a deprecation warning
 I18n.enforce_available_locales = false
 
-[ 'helpers', 'line_item', 'calculator', 'standard_tax', 'import_tax', 'data', 'cli', 'reader', 'file_reader', 'csv_writer' ].each do |file|
+%w(helpers line_item calculator standard_tax import_tax data cli reader file_reader csv_writer').each do |file|
   require File.join(File.dirname(__FILE__), 'sales_tax', file)
 end
 
